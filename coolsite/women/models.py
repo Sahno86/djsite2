c657060b3609d8b7512831a2b8d2f2e2dac8049e
sahno <mishenkinav@gmail.com>
2023-10-22 19:36:14 +0500

    урок 5

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Women(models.Model):
     title = models.CharField(max_length=225)
     content = models.TextField(blank=True)
@@ -7,3 +8,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.title
